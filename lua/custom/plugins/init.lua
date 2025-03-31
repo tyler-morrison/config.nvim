b263f64fb17bac57495ef194266a8cd02d189310
Tyler Morrison <tmorrison@fastmail.com>
2025-03-30 22:40:55 -0400

    feat: adds TypeScript LSP support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,5 @@
 -- See the kickstart.nvim README for more information
 return {
   require 'custom.plugins.transparent',
+  require 'custom.plugins.typescript-tools',
 }
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
--- LSP Pluginsls
+-- LSP Plugins
 return {
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -211,17 +211,6 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
